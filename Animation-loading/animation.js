3dd3d462093e8a97e391694fa23b0b38d7b79925
Bruno Coelho <112569417+BMinority@users.noreply.github.com>
2023-05-24 11:44:43 -0300

    Delete Animation-loading directory

--- Animation-loading/animation.js
+++
@@ -1,7 +0,0 @@
-document.onreadystatechange = function () {
-    if (document.readyState !== "complete") {
-        document.getElementById("gif-container").classList.remove("hidden");
-    } else {
-        document.getElementById("gif-container").classList.add("hidden");
-    }
-};
