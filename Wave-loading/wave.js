938099877718ae2e4a0ab5cc85aea9e443a0fac6
Bruno Coelho <112569417+BMinority@users.noreply.github.com>
2023-05-24 11:45:52 -0300

    Delete Wave-loading directory

--- Wave-loading/wave.js
+++
@@ -1,7 +0,0 @@
-document.onreadystatechange = function () {
-    if (document.readyState !== "complete") {
-        document.getElementById("loading-container").classList.remove("hidden");
-    } else {
-        document.getElementById("loading-container").classList.add("hidden");
-    }
-};
